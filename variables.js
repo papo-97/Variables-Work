5348df5a43f53b025aff9456dc74915cd8b34616
papo-97 <95270166+papo-97@users.noreply.github.com>
2021-12-03 00:38:33 -0800

    i work with variables

---
+++ variables.js
@@ -0,0 +1,27 @@
+//admin copy of name
+let name="John";
+let admin=name ;
+console.log(admin);
+
+// output i and n
+let i=10;
+let n= i++ %5;
+// n=0  ,  i=10 
+
+// transfer
+let a=10,
+b=3,
+c=a;
+a=b;
+b=c;
+//Swap to variables
+let a=10,
+b=3;
+a=a+b;
+b=a-b;
+a=a-b;
+
+
+
+
+
